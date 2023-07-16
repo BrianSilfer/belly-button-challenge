7fd0a0a2b52cfaf059c1d55163e28f9287e5cdcb
BrianSilfer <briansilfer67@gmail.com>
2023-07-15 20:34:01 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -4,46 +4,82 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 //Promise Pending
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
-// //Loading data from URL using D3 and console log for verification
-function init() {
-
-let data = d3.json(url).then(function(data) {
-    console.log(data);
-
-    // define samples for later use
-    let samples = data["samples"][0];
-    console.log(samples["otu_ids"]);
-    console.log(samples["sample_values"]);
-    let otuid = samples["otu_ids"];
-    let samplevals = samples["sample_values"];
-    let trace1  = {
-        x: samplevals,
-        y: otuid,
-        type: 'bar',
-        orientation: 'h'
-    };
-   //Data trace array
-    let barData = [trace1];
-
-    Plotly.newPlot("bar",barData);
-
-    //Bubble Chart
-    let trace2 = {
-        x: otuid,
-        y: samplevals,
-        mode: 'markers',
-        marker: {
-            size: samplevals,
-            color: otuid,
-            text: samples["otu_lables"]
-        }
-    };
-    let bubbleData = [trace2];
-    Plotly.newPlot("bubble",bubbleData);
-});
-
-};
-
-
-
+// find part of html code that will change when the dropdown is selected
+d3.selectAll("#selDataset").on("change", updatePlotly);
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    let dataset = dropdownMenu.property("value");
+    //Loading data from URL using D3 and console log for verification
+    let data = d3.json(url).then(function(data) {
+        console.log(data);
     
+        // for loop to cover every otuid
+        for (let i = 0; i < data.length; i++) {
+            
+    
+            // Initialize x and y arrays for plotting updated chart
+            let x = [];
+            let y = [];
+
+
+            // define samples for later use
+            let samples = data["samples"][i];
+
+            // console.log(samples["otu_ids"]);
+            // console.log(samples["sample_values"]);
+            let otulabels = samples["otu_labels"].slice(0,10);
+            let otuid = samples["otu_ids"].toString().split(',').slice(0,10);
+            let samplevals = samples["sample_values"].slice(0,10);
+            if ("values" === i ) {
+                x = [otuid];
+                y = [samplevals];
+              }
+        };
+        let trace1  = {
+            x: samplevals,
+            y: otuid,
+            orientation: 'h',
+            marker:{width: 10},
+            text: otulabels,
+            type: 'bar'
+
+        };
+        let layout = {
+            yaxis:{
+                title: {text: 'OTU ID'}},
+            xaxis:{
+                title: {text: 'Sample Values'}
+            }};
+        //  console.log(otuid);
+        //Data trace array
+        let barData = [trace1];
+
+        Plotly.newPlot("bar",barData,layout);
+
+//     //Bubble Chart
+//     let trace2 = {
+//         x: otuid,
+//         y: samplevals,
+//         mode: 'markers',
+//         marker: {
+//             size: samplevals,
+//             color: otuid,
+//         },
+//         text: otulabels
+//     };
+//     let layout2 = {
+//         xaxis:{
+//             title: {text: 'OTU ID'}},
+//         yaxis:{ 
+//             title: {text: 'Sample Values'}}
+    
+//     };
+//     let bubbleData = [trace2];
+//     Plotly.newPlot("bubble",bubbleData,layout2);
+
+//     console.log(data["metadata"][0]);
+    
+}) };
